a482f8be142a7c74031b56529c31f6bcdab30c16
satish8211 <satish821@gmail.com>
2025-03-27 10:59:08 +0530

    Add files via upload

---
+++ partition_stack.sql
@@ -0,0 +1,45 @@
+--create and load the data from bigquery public dataset stackoverflow. size - 29gb
+
+create table if not exists b17_dataset.stack_data as select * FROM bigquery-public-data.stackoverflow.stackoverflow_posts
+   
+
+--creating new table with partition  with creation_date cloumn.  
+ CREATE TABLE
+ b17_dataset.stack_data_partition ( 
+ id INTEGER,
+ title STRING,
+ body STRING,
+ accepted_answer_id INTEGER,
+ answer_count INTEGER,
+ comment_count INTEGER,
+ community_owned_date TIMESTAMP,
+ creation_date TIMESTAMP,
+ favorite_count INTEGER,
+ last_activity_date TIMESTAMP,
+ last_edit_date TIMESTAMP,
+ last_editor_display_name STRING,
+ last_editor_user_id INTEGER,
+ owner_display_name STRING,
+ owner_user_id INTEGER,
+ parent_id INTEGER,
+ post_type_id INTEGER,
+ score INTEGER,
+ tags STRING,
+ view_count INTEGER )
+PARTITION BY date(creation_date)
+  cluster by id,title,body;
+  
+  
+ -- PARTITION BY
+ --date(creation_date);
+  
+--load data from non partition table to new partition table   
+insert into 'b17_dataset.stack_data_partition' select * from b17_dataset.stack_data;
+  
+  
+-- bellow select queries will show the diff b/w partition and non parition table
+
+select * from  b17_dataset.stack_data where date(creation_date) = '2013-11-19';
+
+select * from b17_dataset.stack_data_partition where date(creation_date) = '2013-11-19';
+
